66665dc74e3698e0d9828fed8a934ac09115f016
vidushahasindi <hasindividusha@gmail.com>
2025-04-03 16:58:48 +0530

    Designing database

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,15 +11,47 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //Create users table
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->boolean('is_admin')->default(false); //For role based access control
             $table->timestamps();
         });
+
+        //Create brand table
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+
+        //Create category table
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+
+        //Create item table
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('brand_id')->constrained('brands');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('code');
+            $table->string('name');
+            $table->string('attachment')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
