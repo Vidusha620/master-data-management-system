f4a6e447e5b71b136c2e4a89bba742a422fdd404
vidushahasindi <hasindividusha@gmail.com>
2025-04-03 17:22:00 +0530

    Modifying Models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    protected $fillable = [
+        'brand_id',
+        'category_id',
+        'code',
+        'name',
+        'attachment',
+        'status',
+    ];
 }
